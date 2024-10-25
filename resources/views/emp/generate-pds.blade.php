1c2b26a414683523130b9390b8267866ba1a0052
edz1433 <edzavril1@gmail.com>
2024-10-25 11:42:16 +0800

    update

--- resources/views/emp/generate-pds.blade.php
+++ resources/views/emp/generate-pds.blade.php
@@ -191,7 +191,7 @@
             <thead>
                 <tr>
                     <th colspan="9" class="bg1">
-                        <img src="{{ asset('Uploads/pds-header.png') }}" width="100.1%" alt="" srcset="">
+                        <img src="{{ public_path('Uploads/pds-header.png') }}" width="100.1%" alt="" srcset="">
                     </th>
                 </tr>
             </thead>
