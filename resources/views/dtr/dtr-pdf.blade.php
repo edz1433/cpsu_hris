467a19708556564362f1bfd187791689097b2942
edz1433 <edzavril1@gmail.com>
2024-11-18 09:13:36 +0800

    update

--- resources/views/dtr/dtr-pdf.blade.php
+++ resources/views/dtr/dtr-pdf.blade.php
@@ -280,7 +280,7 @@
             <span class="font" style="relative; display: inline-block; width: 100%; text-align: center; margin-top: -37px;">over Printed Name</span>
         </div>
         <p style="font-size: 8px; text-align: center;">
-            Doc Control Code: CPSU-F-HRMO-03     Effective Date:  09/12/2018     Page No:  1 of 1
+            Doc Control Code: CPSU-F-HRMO-03 Effective Date:  09/12/2018     Page No:  1 of 1
         </p>
     </div>
     <div class="column2"> 
@@ -450,7 +450,7 @@
             <span class="font" style="relative; display: inline-block; width: 100%; text-align: center; margin-top: -37px;">over Printed Name</span>
         </div>
         <p style="font-size: 8px; text-align: center;">
-            Doc Control Code: CPSU-F-HRMO-03     Effective Date:  09/12/2018     Page No:  1 of 1
+            Doc Control Code: CPSU-F-HRMO-03 Effective Date:  09/12/2018     Page No:  1 of 1
         </p>
     </div>
 </body>
