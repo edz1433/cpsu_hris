52bf858e715738c26418c2008db6fbb23e2ae438
edz1433 <edzavril1@gmail.com>
2024-12-02 12:11:37 +0800

    update

--- resources/views/verify.blade.php
+++ resources/views/verify.blade.php
@@ -57,13 +57,13 @@
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    {{-- <script>
+    <script>
         $(document).ready(function () {
             var email = $('#email').val();
             if(email == ""){
                 window.location.href = "{{ route('getLogin') }}";
             }
         });
-    </script> --}}
+    </script>
 </body>
 </html>
