7b96e8beddf7f1ac7924595633eb31520134d4f2
edz1433 <edzavril1@gmail.com>
2025-04-25 14:23:15 +0800

    update

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -64,7 +64,7 @@ class EventController extends Controller
             $log->save();
         
             return response()->json([
-                'fullname' => $empid
+                'fullname' => $encryptedempid
             ]);
         }
     }
