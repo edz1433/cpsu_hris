bf6dca70856096e99240c052a594277f5f30e1fa
edz1433 <edzavril1@gmail.com>
2025-06-24 09:12:43 +0800

    update

--- config/app.php
+++ config/app.php
@@ -188,7 +188,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         
         // ✅ Add your line here:
         App\Providers\GlobalViewServiceProvider::class,
