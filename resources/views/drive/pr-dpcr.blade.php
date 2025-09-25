90a790acefb016fd77953d11f0e97a8ac48df523
edz1433 <edzavril1@gmail.com>
2025-09-25 14:34:20 +0800

    update

--- resources/views/drive/pr-dpcr.blade.php
+++ resources/views/drive/pr-dpcr.blade.php
@@ -144,8 +144,8 @@
             <div class="input-group input-group-sm" style="width: auto;">
                 <select class="form-control" id="categorySelect">
                     <option value="0" {{ ($cat == 0) ? 'selected' : '' }}>All</option>
-                    <option value="1" {{ ($cat == 1) ? 'selected' : '' }}>1st Quarter</option>
-                    <option value="2" {{ ($cat == 2) ? 'selected' : '' }}>2nd Quarter</option>
+                    <option value="1" {{ ($cat == 1) ? 'selected' : '' }}>1st Half</option>
+                    <option value="2" {{ ($cat == 2) ? 'selected' : '' }}>2nd Half</option>
                 </select>
                 <div class="input-group-append">
                     <span class="input-group-text"><i class="fas fa-filter"></i></span>
