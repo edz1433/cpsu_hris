e02a03264fe36b9b2eaa3a9059bfd50248bc5135
edz1433 <edzavril1@gmail.com>
2025-11-10 15:38:10 +0800

    update

--- resources/views/script/masterScript.blade.php
+++ resources/views/script/masterScript.blade.php
@@ -1,43 +1,35 @@
-<!-- jQuery -->
-<script src="{{ asset('app.js') }}"></script>
-<!-- jQuery -->
+
 <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('template/dist/js/adminlte.min.js') }}"></script>
 
-<!-- Toastr -->
-<script src="{{ asset('template/plugins/toastr/toastr.min.js') }}"></script>
-<!-- SweetAlert2 -->
-<script src="{{ asset('template/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script defer src="{{ asset('template/dist/js/adminlte.min.js') }}"></script>
 
-<!-- DataTables  & Plugins -->
-<script src="{{ asset('template/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script> 
-<script src="{{ asset('template/plugins/jszip/jszip.min.js') }}"></script>
-<script src="{{ asset('template/plugins/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ asset('template/plugins/pdfmake/vfs_fonts.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-<script src="{{ asset('template/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/toastr/toastr.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/select2/js/select2.full.min.js') }}"></script>
 
-<!-- Select2 -->
-<script src="{{ asset('template/plugins/select2/js/select2.full.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/chart.js/Chart.min.js') }}"></script>
 
-<!-- ChartJS -->
-<script src="{{ asset('template/plugins/chart.js/Chart.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 
-<!-- jQuery UI -->
-<script src="{{ asset('template/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/moment/moment.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/fullcalendar/main.js') }}"></script>
 
-<!-- fullCalendar 2.2.5 -->
-<script src="{{ asset('template/plugins/moment/moment.min.js') }}"></script>
-<script src="{{ asset('template/plugins/fullcalendar/main.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/jszip/jszip.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script defer src="{{ asset('template/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
+<script defer src="{{ asset('app.js') }}"></script>
 
 {{-- @include('script.dashboardChart') --}}
 {{-- Notification --}}
@@ -464,5 +456,10 @@ $(document).ready(function () {
             }
         });
     });
+});
+$(function() {
+  $("input[data-bootstrap-switch]").each(function() {
+    $(this).bootstrapSwitch('state', $(this).prop('checked'));
+  });
 });
 </script>
