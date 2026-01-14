f05dd47d9d172ea360e2d9e6e410327798e20dd6
edz1433 <edzavril1@gmail.com>
2026-01-14 14:45:35 +0800

    update

--- resources/views/emp/generate-pds.blade.php
+++ resources/views/emp/generate-pds.blade.php
@@ -304,7 +304,7 @@
                 </tr>
                 <tr>
                     <td class="bordered pl2 bg2">11. PAG-IBIG ID NO.</td>
-                    <th class="bordered pl2" colspan="2">{{ $datas['employee']->pagibig ?? 'N/A' }}</th>
+                    <th class="bordered pl2" colspan="2">{{ $datas['employee']->sss ?? 'N/A' }}</th>
                     <td class="bordered pl2" colspan="5">
                         <div class="address-column">
                             <b>{{ isset($datas['city1']->name) ? strtoupper($datas['city1']->name) : 'N/A' }}</b><br>City/Municipality
