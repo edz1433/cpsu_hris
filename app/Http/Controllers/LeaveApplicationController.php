456c1aab99a263d67b6b38bdc51b7ea2c04a1780
edz1433 <edzavril1@gmail.com>
2026-01-26 09:15:09 +0800

    update

--- app/Http/Controllers/LeaveApplicationController.php
+++ app/Http/Controllers/LeaveApplicationController.php
@@ -188,18 +188,14 @@ class LeaveApplicationController extends Controller
         if ($setting->suc_pres !== auth()->guard($guard)->user()->id) {
             if ($oic == null) {
                 $leavesapphead->where('leave_applications.supervisor', auth()->guard($guard)->user()->id);
-                $leavesapphead->where('sup.emp_dept', auth()->guard($guard)->user()->emp_dept);
-                $leavesapphead->where('emp.emp_dept', auth()->guard($guard)->user()->emp_dept);
-
+                $leavesapphead->where('employees.emp_dept', auth()->guard($guard)->user()->emp_dept);
             }else{
                 $leavesapphead->where('leave_applications.empid', '!=', $employee->emp_ID);
-                $leavesapphead->where('sup.emp_dept', auth()->guard($guard)->user()->emp_dept);
-                $leavesapphead->where('emp.emp_dept', auth()->guard($guard)->user()->emp_dept);
+                $leavesapphead->where('employees.emp_dept', auth()->guard($guard)->user()->emp_dept);
             }
         }else{
             $leavesapphead->whereIn('leave_applications.status', [3]);
-            $leavesapphead->where('sup.emp_dept', auth()->guard($guard)->user()->emp_dept);
-            $leavesapphead->where('emp.emp_dept', auth()->guard($guard)->user()->emp_dept);
+            $leavesapphead->where('employees.emp_dept', auth()->guard($guard)->user()->emp_dept);
         }
         
         $leavesapphead = $leavesapphead->select(
